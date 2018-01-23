a5f99a3eabeaa8598fef37c6e6ae6be40990cab6
lidawei <mynameislidawei@gmail.com>
2018-01-23 14:40:08 +0800

    update debug header file

--- util-debug.h
+++ util-debug.h
@@ -1,12 +1,12 @@
 #ifndef __UTIL_DEBUG_H__
 #define __UTIL_DEBUG_H__
-
+#include <stdio.h>
 #ifndef DEBUG_PRINT
-#define DEBUG_PRINT(fmt, arg...) printf("%s "fmt, __func__, ##arg)
+#define DEBUG_PRINT(fmt, arg...) printf("%s " fmt, __func__, ##arg)
 #endif /*DEBUG_PRINT*/
 
 #ifndef ERROR_PRINT
-#define ERROR_PRINT(fmt, arg...) fprintf(stderr, "%s:%d "fmt, __FILE__, __LINE__, ##arg)
+#define ERROR_PRINT(fmt, arg...) fprintf(stderr, "%s:%d " fmt, __FILE__, __LINE__, ##arg)
 #endif /*ERROR_PRINT*/
 
 
