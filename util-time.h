49fcd6e74f4be71164802560acaf9735e581f1b1
lidawei <mynameislidawei@gmail.com>
2018-08-21 11:34:34 +0800

    provide Windows, Linux, MacOS util-time.h

--- util-time.h
+++ util-time.h
@@ -1,10 +1,37 @@
 #ifndef __UTIL_TIME_H__
 #define __UTIL_TIME_H__
 
-#include <stdint.h>
 #include <time.h>
-#include <sys/time.h>
+#include <stdint.h>
 #include <string>
+
+#ifdef _WIN32
+#include <windows.h>
+int gettimeofday(struct timeval *tv, void *tz);
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef _WIN32
+int gettimeofday(struct timeval *tv, void *tz)
+{
+    time_t clock;
+    struct tm tm; 
+    SYSTEMTIME wtm;
+    GetLocalTime(&wtm);
+    tm.tm_year = wtm.wYear - 1900;
+    tm.tm_mon = wtm.wMonth - 1;
+    tm.tm_mday = wtm.wDay;
+    tm.tm_hour = wtm.wHour;
+    tm.tm_min = wtm.wMinute;
+    tm.tm_sec = wtm.wSecond;
+    tm.tm_isdst = -1; 
+    clock = mktime(&tm);
+    tv->tv_sec = clock;
+    tv->tv_usec = wtm.wMilliseconds * 1000;
+    return 0;
+}
+#endif
 
 namespace utils {
 
